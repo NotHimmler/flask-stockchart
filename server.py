a8d7db520511e23fd031b9e3d6e07977675a62ab
Heinrich Malan <heinrichmalan@hotmail.com>
2017-02-05 22:34:04 +1100

    changed data route to send false json for non-existent stock ticker

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_from_directory
 import importlib
+import json
 gf = importlib.import_module("google_finance")
 
 __app__ = Flask(__name__)
@@ -11,9 +12,13 @@ def server():
 @__app__.route("/get_data/", defaults={'path':''})
 @__app__.route("/get_data/<path:path>")
 def get_data(path):
-    gf.get_historical_data(path)
-    gf.json_from_csv(path)
-    return send_from_directory('./static/json','{0}.json'.format(path))
-
+    data_exists = gf.get_historical_data(path)
+    
+    if(data_exists):
+        gf.json_from_csv(path)
+        return send_from_directory('./static/json','{0}.json'.format(path))
+    else:
+        return json.dumps(False)
+    
 if __name__ == "__main__":
     __app__.run(debug=True)
